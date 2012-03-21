14a0ffba974c406e55a092204b7c12dcf4454cb7
Fabian Sorqvist <fabian.sorqvist@gmail.com>
2012-03-20 18:06:42 -0600

    Rewrite javascript autocomplete functionality to work with multiple add forms on the same page

--- js/entity_autocomplete.js
+++ js/entity_autocomplete.js
@@ -2,29 +2,30 @@
 Drupal.behaviors.entitycollectionAdmin = {
   attach: function (context, settings) {
     var acdb = [];
-    var path = Drupal.settings.EntityCollection.path;
-    $('#edit-entity-type', context).change(attachAutocomplete);
-    $('#edit-content-select', context).once('autocomplete', function() {
-      var $input = $('#edit-content-select')
-        .attr('autocomplete', 'OFF')
-        .attr('aria-autocomplete', 'list');
-      $($input[0].form).submit(Drupal.autocompleteSubmit);
-      $input.parent()
-        .attr('role', 'application')
-        .append($('<span class="element-invisible" aria-live="assertive"></span>')
-          .attr('id', $input.attr('id') + '-autocomplete-aria-live')
-      );
-      attachAutocomplete();
+    var entity_collection_forms = Drupal.settings.entity_collection;
+    $.each(entity_collection_forms, function(entity_collection, form_settings) {
+      var path = form_settings.path;
+      var form = $('form.' + entity_collection, context);
+      var input = $("input[name='content_select']", form)
+      input.once('autocomplete', function() {
+    	input.change(attachAutocomplete);
+        input.attr('autocomplete', 'OFF').attr('aria-autocomplete', 'list');
+        $(input[0].form).submit(Drupal.autocompleteSubmit);
+        input.parent()
+          .attr('role', 'application')
+          .append($('<span class="element-invisible" aria-live="assertive"></span>')
+          .attr('id', input.attr('id') + '-autocomplete-aria-live')
+        );
+        attachAutocomplete(path  + '/' +  $("select[name='entity_type']", form).val(), input);
+      });
     });
 
-    function attachAutocomplete() {
-      var uri = path  + '/' +  $('#edit-entity-type').val();
-      var $input = $('#edit-content-select');
+    function attachAutocomplete(uri, input) {
       if (!acdb[uri]) {
         acdb[uri] = new Drupal.ACDB(uri);
       }
-      $input.unbind();
-      new Drupal.jsAC($input, acdb[uri]);
+      input.unbind();
+      new Drupal.jsAC(input, acdb[uri]);
     }
   }
 }
