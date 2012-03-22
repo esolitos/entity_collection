c207faf069abb319855c62ff88c390c550722478
Fabian Sorqvist <fabian.sorqvist@gmail.com>
2012-03-22 15:39:27 -0600

    Hierarchy template file

---
+++ entity_collection_style_hierarchy.tpl.php
@@ -0,0 +1,12 @@
+<div class="group">
+	<?php if (!empty($parent)): ?>
+	<div class="parent">
+		<?php print render($parent) ?>
+	</div>
+	<?php endif; ?>
+	<?php if (!empty($children)): ?>
+	<div class="children">
+		<?php print render($children) ?>
+	</div>
+	<?php endif; ?>
+</div>
