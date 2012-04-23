8bc05b83280a5299799158489e4bf0e949f726a2
Fabian Sorqvist <fabian.sorqvist@gmail.com>
2012-04-23 10:26:32 +0200

    Make the hierarchy plugin work.

--- entity_collection_style_hierarchy.tpl.php
+++ entity_collection_style_hierarchy.tpl.php
@@ -5,14 +5,14 @@
  */
 ?>
 <div class="group">
-	<?php if (!empty($parent)): ?>
-	<div class="parent">
-		<?php print render($parent) ?>
-	</div>
-	<?php endif; ?>
-	<?php if (!empty($children)): ?>
-	<div class="children">
-		<?php print render($children) ?>
-	</div>
-	<?php endif; ?>
+  <?php if (!empty($parent)): ?>
+    <div class="parent">
+      <?php print render($parent) ?>
+    </div>
+  <?php endif; ?>
+  <?php if (!empty($children)): ?>
+    <div class="children">
+      <?php print render($children) ?>
+    </div>
+  <?php endif; ?>
 </div>
