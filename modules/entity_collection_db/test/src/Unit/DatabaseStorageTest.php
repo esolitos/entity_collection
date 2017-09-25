300d9b3ef1de1843058700eced9eeb4a2a2adf20
Marlon Saglia <esolitos@gmail.com>
2017-09-25 13:51:45 +0200

    Add empty test

---
+++ modules/entity_collection_db/test/src/Unit/DatabaseStorageTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Drupal\Tests\entity_collection_db\Unit;
+
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * Tests Database storage backend for EntityCollection functionality.
+ *
+ *
+ * @coversDefaultClass \Drupal\entity_collection_db\Plugin\EntityCollection\Storage\Database
+ * @group EntityCollectionEntityTest
+ */
+class DatabaseStorageTest extends UnitTestCase {
+
+  public function test true() {
+
+    $this->assertTrue(FALSE, "working");
+  }
+}
