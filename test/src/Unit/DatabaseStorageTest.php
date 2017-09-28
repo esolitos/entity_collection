12e5b2ace0c75b3695dc011897d8dca554029d8e
Marlon Saglia <esolitos@gmail.com>
2017-09-28 19:55:35 +0200

    Add placeholder test

---
+++ test/src/Unit/DatabaseStorageTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Drupal\Tests\entity_collection\Unit;
+
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * Tests Database storage backend for EntityCollection functionality.
+ *
+ *
+ * @coversDefaultClass \Drupal\entity_collection\Plugin\EntityCollection\Storage\Database
+ * @group EntityCollectionEntityTest
+ */
+class DatabaseStorageTest extends UnitTestCase {
+
+  // @TODO Placeholder test
+  public function test true() {
+    $this->assertTrue(TRUE);
+  }
+}
